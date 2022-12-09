2bdb7a9b22d404acf77c27345c76802d3e58d31d
Dan McKinley <mcfunley@gmail.com>
2022-12-09 11:42:55 -0800

    q9

--- 2022/q9.py
+++ 2022/q9.py
@@ -2,22 +2,24 @@
 import numpy as np
 
 data = open('data/q9.dat', 'r').readlines()
-moves = ((d, int(n)) for d, n in (l.strip().split(' ') for l in data))
+moves = [(d, int(n)) for d, n in (l.strip().split(' ') for l in data)]
+directions = { 'R': [1, 0], 'U': [0, -1], 'D': [0, 1], 'L': [-1, 0], }
 
-def tailmove(t, h):
+def follow_move(t, h):
     if max(abs(h - t)) <= 1:
         return [0, 0]
     return [np.sign(d) if abs(d) > 1 else d for d in h - t]
 
-h = np.zeros(2)
-t = np.zeros(2)
-tailpositions = { tuple(t) }
-directions = { 'R': [1, 0], 'U': [0, -1], 'D': [0, 1], 'L': [-1, 0], }
+def run(knots):
+    segments = [np.zeros(2) for _ in range(knots)]
+    tail_positions = { tuple(segments[-1]) }
+    for d, n in moves:
+        for _ in range(n):
+            segments[0] += directions[d]
+            for i in range(1, knots):
+                segments[i] += follow_move(segments[i], segments[i-1])
+            tail_positions.add(tuple(segments[-1]))
+    print(len(tail_positions))
 
-for d, n in moves:
-    for _ in range(n):
-        h += directions[d]
-        t += tailmove(t, h)
-        tailpositions.add(tuple(t))
-
-print(len(tailpositions))
+run(2)
+run(10)
