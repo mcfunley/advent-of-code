85bc538b578263bba692dba9302b65b13fedba6a
Dan McKinley <mcfunley@gmail.com>
2024-12-07 09:53:18 -0800

    q7

---
+++ 2024/q7b.py
@@ -0,0 +1,28 @@
+from tqdm import tqdm
+
+data = open("data/p7.txt", "r").readlines()
+
+equations = [
+    [int(s), list(map(int, t.split()))]
+    for s, t in (line.split(": ") for line in data)
+]
+
+
+def concat(a, b):
+    return int(str(a) + str(b))
+
+
+def solve(acc, result, numbers):
+    if len(numbers) == 0:
+        return result if acc == result else 0
+    n, *rest = numbers
+    return max(
+        [
+            solve(acc + n, result, rest),
+            solve(acc * n, result, rest),
+            solve(concat(acc, n), result, rest),
+        ]
+    )
+
+
+print(sum(tqdm((solve(0, *eq) for eq in equations), total=len(equations))))
